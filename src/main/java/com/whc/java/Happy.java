00bec9e83151fe3b5aa58165105fc993a68322e7
whc <angaoen@outlook.com>
2020-04-30 10:55:00 +0800

    more edit.

--- src/main/java/com/whc/java/Happy.java
+++ src/main/java/com/whc/java/Happy.java
@@ -1,9 +1,10 @@
 package com.whc.java;
 
 /**
- * @author wanghaochen
- *2020年4月30日上午10:53:03
+ * @author wanghaochen 2020年4月30日上午10:53:03
  */
 public class Happy {
-
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 }
